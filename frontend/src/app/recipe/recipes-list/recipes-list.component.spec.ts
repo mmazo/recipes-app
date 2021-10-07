7dd478fecc527bcba029cfd71a074711239aa0e3
Mazo Michail (IOC/PAP-CO) <Michail.Mazo@bosch.io>
2021-10-07 21:02:55 +0200

    added recipes list test

--- frontend/src/app/recipe/recipes-list/recipes-list.component.spec.ts
+++ frontend/src/app/recipe/recipes-list/recipes-list.component.spec.ts
@@ -3,10 +3,18 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RecipesListComponent } from './recipes-list.component';
 import { RecipeService } from '../recipe.service';
 import { of } from 'rxjs';
+import { Recipe } from '../recipe';
+
+const mockedRecipesList: Array<Recipe> = [{
+  recipeId: 1,
+  name: 'Pizza',
+  description: 'Mega Pizza',
+  imageId: 23
+}];
 
 class MockRecipeService {
   getList(){
-    return of([]);
+    return of(mockedRecipesList);
   }
 }
 
@@ -30,5 +38,11 @@ describe('RecipesListComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render title', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.card-title')?.textContent).toContain(mockedRecipesList[0].name);
+    expect(compiled.querySelector('.card-text')?.textContent).toContain(mockedRecipesList[0].description);
   });
 });
