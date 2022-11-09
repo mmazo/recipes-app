73e5e9d6db223c24024290826e1725dc43b92902
Mazo Michail (IOB/PAD-DDA-CO) <Michail.Mazo@bosch.io>
2022-11-09 09:59:14 +0100

    started with graphql and added first graphql endpoint for tags

---
+++ backend/src/main/java/de/mmazo/mealsplan/tag/TagController.java
@@ -0,0 +1,20 @@
+package de.mmazo.mealsplan.tag;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class TagController {
+
+    private final TagRepository tagRepository;
+
+    @Autowired
+    public TagController(TagRepository tagRepository){ this.tagRepository = tagRepository; }
+
+    @SchemaMapping(typeName = "Query",value = "allTags")
+    public Iterable<Tag> findAll() {
+        return tagRepository.findAll();
+    }
+
+}
